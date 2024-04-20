64e26c21715a8431330f90a075318188eccf8fea
Goldensquirrel <goldensquirrel11@gmail.com>
2024-04-20 20:34:23 +0800

    Created ping command

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -9,12 +9,23 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 def main():
     intents = discord.Intents.default()
+    intents.message_content = True
 
     bot = commands.Bot(command_prefix="$", intents=intents)
 
     @bot.event
     async def on_ready():
         print("Bot has started with username:", bot.user)
+
+    @bot.command(
+            aliases=['p'],
+            help="",
+            description="",
+            brief="Responds with pong",
+            enabled=True
+    )
+    async def ping(ctx):
+        await ctx.send("pong")
 
     bot.run(DISCORD_TOKEN)
 
